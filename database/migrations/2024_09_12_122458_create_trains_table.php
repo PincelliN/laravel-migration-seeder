9267f5948ff18ec9a2a9f9eff18c3bfa3b7d3e5b
Nicolò Pincelli <pincelli.n@gmail.com>
2024-09-12 17:36:34 +0200

    Add lunghezza dato

--- database/migrations/2024_09_12_122458_create_trains_table.php
+++ database/migrations/2024_09_12_122458_create_trains_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            $table->string('azienda');
-            $table->string('stazione_di_partenza');
-            $table->string('stazione_di_arrivo');
+            $table->string('azienda',20);
+            $table->string('stazione_di_partenza',50);
+            $table->string('stazione_di_arrivo',50);
             $table->time('partenza');
             $table->time('arrivo');
             $table->string('codice',10);
